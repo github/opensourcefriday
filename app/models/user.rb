c5c56148f883f1e9fc2d1d9b746d94d3df89063c
Ben Polinsky <benjamin.polinsky@gmail.com>
2017-01-13 08:50:24 -0500

    basic auth: devise + signin/signup with github

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and 
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, 
+         omniauth_providers: [:github]
+         
+  def self.github_auth(auth)
+    where(github_uid: auth.uid).first_or_create do |user|
+      user.github_uid = auth.uid
+      user.github_username = auth.info.nickname
+      user.github_name = auth.info.name
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
+end
