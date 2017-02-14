2223dcba6e88c3c869316f826e73b1fdf0902fdd
Mike McQuaid <mike@mikemcquaid.com>
2017-02-14 17:01:26 +0000

    Add page content.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,16 @@
 class HomeController < ApplicationController
+  def contributors
+    @subtitle = "Contributors"
+    render :contributors
+  end
 
+  def maintainers
+    @subtitle = "Maintainers"
+    render :maintainers
+  end
+
+  def companies
+    @subtitle = "Companies"
+    render :companies
+  end
 end
