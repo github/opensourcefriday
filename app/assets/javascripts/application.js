f8113cd6cc1f0cb661dc0736c490045c121c12b9
Mike McQuaid <mike@mikemcquaid.com>
2017-05-08 19:00:47 +0100

    Move calendar to after user sign-in
    Also:
    - Use `jstimezonedetect` to adjust the calendar to the user’s timezone
    - Use `addtocalendar` from `npm` rather than an external source
    - Redirect to the user’s page after sign-in (where they’ll see the
    calendar)
    - Unify the calendar code into a partial
    - Note that the calendar is for the next Open Source Friday rather than
    a recurring event (recurring is unsupported by `addtocalendar` and it
    sounds like it’d be annoyingly hard to do ourselves)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jstimezonedetect/dist/jstz
 //= require_tree .
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user.github_username)
+  end
 end
