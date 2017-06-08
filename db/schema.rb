990267fff5c05ffaf3d170c5af1265c625c60dcd
Katrina Owen <kytrinyx@github.com>
2017-06-08 15:07:43 -0600

    Add migration to track newsletter subscriptions
    The actual subscription is tracked in MailChimp, however we need to know whether or not to
    display the form.
    
    I don't want to have to ask MailChimp about their subscription every time just to show
    the page.

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420081909) do
+ActiveRecord::Schema.define(version: 20170608210301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                    default: "",    null: false
+    t.string   "encrypted_password",       default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer  "sign_in_count",            default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
@@ -29,9 +29,10 @@ ActiveRecord::Schema.define(version: 20170420081909) do
     t.string   "github_uid"
     t.string   "github_username"
     t.string   "github_name"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.string   "oauth_token"
+    t.boolean  "subscribed_to_newsletter", default: false
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
