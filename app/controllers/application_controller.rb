17f3df2907bbb1b3b91473a1e0620f78160868e6
Mike McQuaid <mike@mikemcquaid.com>
2017-08-14 16:57:55 +0100

    application_controller: tweak formatting.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,22 @@ class ApplicationController < ActionController::Base
   before_action :set_language
 
   def set_language
-    I18n.locale =
-      if user_signed_in?
-        current_user.language
-      else
-        determine_language
-      end
+    I18n.locale = if user_signed_in?
+      current_user.language
+    else
+      determine_language
+    end
   end
 
   def default_url_options(*)
-    { locale:
-        if !I18n.locale || !available_language?(I18n.locale) ||
-           I18n.locale == I18n.default_locale # omit when default
-          nil
-        else
-          I18n.locale
-        end, }
+    locale = if I18n.locale && available_language?(I18n.locale) &&
+                I18n.locale != I18n.default_locale
+      I18n.locale
+    end
+    
+    {
+      locale: locale,
+    }
   end
 
   def after_sign_in_path_for(_resource)
@@ -29,16 +29,16 @@ class ApplicationController < ActionController::Base
 
   def determine_language
     if params[:locale].present? && available_language?(params[:locale])
-      params[:locale]
-    else
-      language = request.env["HTTP_ACCEPT_LANGUAGE"] || ""
-      language = language.scan(/^[a-z]{2}/).first
-      if available_language?(language)
-        language
-      else
-        I18n.default_locale
-      end
+      return params[:locale]
     end
+    
+    language = request.env["HTTP_ACCEPT_LANGUAGE"] || ""
+    language = language.scan(/^[a-z]{2}/).first
+    if available_language?(language)
+      return language
+    end
+    
+    I18n.default_locale
   end
 
   def available_language?(language)
