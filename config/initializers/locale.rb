105b33d849d026884480aa5f0f3c7cfdf7eea304
Mike McQuaid <mike@mikemcquaid.com>
2019-12-27 10:42:32 +0000

    Fix Korean translation
    Fix the language code and use the same one from Transifex.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,4 +1,4 @@
 # Whitelist locales available for the application
-I18n.available_locales = %i[en es fr ja zh kr ru uk]
+I18n.available_locales = %i[en es fr ja zh ko ru uk]
 
 I18n.default_locale = :en
